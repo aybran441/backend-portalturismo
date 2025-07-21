d0b0b43d0d85a471fa441d2bbd7c11292e488db0
aybran441 <983558@senacminas.edu.br>
2025-07-21 15:23:00 -0300

    UPDATE

---
+++ routes/userRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const userController = require('../controllers/userController')
+const User = require('../models/Users');
+ 
+// ✅ Criar usuário (registro) — público
+router.post('/', userController.createUser);
+ 
+// ✅ Listar todos os usuários
+router.get('/', userController.listUser);
+ 
+// ✅ Buscar usuário por ID
+router.get('/:id', userController.listUserById);
+ 
+// ✅ Atualizar usuário
+router.put('/:id', userController.updateUser);
+ 
+// ✅ Deletar usuário
+router.delete('/:id', userController.deleteUser);
+ 
+module.exports = router;
+ 
