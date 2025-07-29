516e6400056347c7afc5ab688ef042dca4fb1963
aybran441 <983558@senacminas.edu.br>
2025-07-29 16:10:18 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express'); // Importa o framework Express
 const router = express.Router();    // Cria uma instância do roteador do Express
-const auth = require('../controllers/AuthController'); // Importa o controller responsável pela autenticação
+const auth = require('../controllers/authController'); // Importa o controller responsável pela autenticação
 
 // Define uma rota POST para '/login' que chama a função 'login' do controller 'auth'
 router.post('/login', auth.login);
