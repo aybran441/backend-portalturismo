1b2a296d25aa71b881274996cf01abb728a5352b
aybran441 <983558@senacminas.edu.br>
2025-08-01 14:35:17 -0300

    update

--- config/db.js
+++ config/db.js
@@ -1,20 +1,16 @@
-// Importa a classe Sequelize do pacote sequelize para configurar a conexão com o banco de dados
-const { Sequelize } = require('sequelize')
-
-// Importa o pacote dotenv para carregar variáveis de ambiente do arquivo .env
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
-
-// Cria uma nova instância do Sequelize, configurando a conexão com o banco MySQL
-const sequelize = new Sequelize(
-    process.env.DB_NAME,      // Nome do banco de dados, obtido das variáveis de ambiente
-    process.env.DB_USER,      // Usuário do banco, obtido das variáveis de ambiente
-    process.env.DB_PASSWORD,  // Senha do banco, obtida das variáveis de ambiente
-    {
-        host: process.env.DB_HOST,  // Endereço do servidor do banco de dados
-        dialect: 'mysql',           // Dialeto do banco, aqui MySQL
-        logging: false              // Desativa logs SQL no console para não poluir a saída
+ 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // importante para conexões com SSL (Render exige isso)
     }
-)
-
-// Exporta a instância do Sequelize para ser usada em outras partes da aplicação
+  },
+  logging: false,
+});
+ 
 module.exports = sequelize;
